d5808436ae963c0cc968622d732e5ebbc6d7de8a
shinyerang <syr20201103@gmail.com>
2023-06-16 10:25:15 +0900

    로칼

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ st.video("https://www.youtube.com/watch?v=SaCheA6Njc4")  # 유튜브 링크
 st.image("https://cdn.pixabay.com/photo/2023/06/02/14/12/woman-8035772_1280.jpg")  # 인터넷 링크
 st.image("https://i.imgur.com/jorp5JH.png")  # 인터넷 링크
 # 여러 가지 옵션을 넣어서 세부 기능들을 차이
-#st.image("img/img.png")  # 파일 경로 (app.py)
-#st.image(image="img/img.png")  # 키워드를 사용해서...
-#st.image("img/img.png", use_column_width=True)  # 파일 경로 (app.py)
-#st.image("img/img.png", width=100)  # 파일 경로 (app.py)
+st.image("img/img.png")  # 파일 경로 (app.py)
+st.image(image="img/img.png")  # 키워드를 사용해서...
+st.image("img/img.png", use_column_width=True)  # 파일 경로 (app.py)
+st.image("img/img.png", width=100)  # 파일 경로 (app.py)
 # https://imgur.com/
